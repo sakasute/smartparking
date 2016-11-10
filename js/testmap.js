af207c7256e3035ae2882dbfb319006123776c4c
Sakari Sutela <sakari.sutela@gmail.com>
2016-11-10 22:19:00 +0200

    some practice with leaflet: realtime HSL-bus locations

--- js/testmap.js
+++ js/testmap.js
@@ -6,17 +6,44 @@ var plotlayers=[];
 function initmap() {
 	// set up the map
 	map = new L.Map('testmap');
+	markers = [];
 
 	// create the tile layer with correct attribution
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 20, attribution: osmAttrib});
 
-	// start the map in South-East England
+	// start the map in Helsinki
 	map.setView(new L.LatLng(60.1826, 24.9215),12);
 	map.addLayer(osm);
 }
 
+function refreshBusLocations() {
+	console.log("tick")
+	var locations = [];
+	$.each(markers, function(index, value) {
+		map.removeLayer(value);
+	});
+
+	$.getJSON("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL", function( data ) {
+		vehicleActivity = data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity;
+
+		$.each(vehicleActivity, function(index, value) {
+			locations.push([value.MonitoredVehicleJourney.VehicleLocation.Latitude, value.MonitoredVehicleJourney.VehicleLocation.Longitude]);
+		});
+
+		$.each(locations, function(index, value) {
+			markers.push(L.marker(value).addTo(map));
+		});
+
+	})
+
+
+}
+
 $( document ).ready(function() {
     initmap();
+		refreshBusLocations();
+		setInterval(refreshBusLocations, 5000);
+
 });
