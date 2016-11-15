3b22677ad7b8bf17d7614f9f9cc32fd4051a7e26
Sakari Sutela <sakari.sutela@gmail.com>
2016-11-15 15:19:25 +0200

    get_json_files-skriptin tuunailuu

--- pythonscripts/get_json_files.py
+++ pythonscripts/get_json_files.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 '''
 Created on Nov 15, 2016
 
 @author: sakari
 '''
+
+
 
 import requests
 import time
@@ -10,10 +14,10 @@ import time
 while 1:
     resp = requests.get('http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL')
 
-    current_time = "%10d"%(time.time())
+    current_time = "%10d"%(time.time())  # tämän hetken timestamp sekunteina tiedoston nimeä varten
     
-    output_file = open(current_time + ".json", 'w+')
+    output_file = open("hsl_data/" + current_time + ".json", 'w+')  # muista luoda hsl_data -kansio ennen scriptin ajamista
     
     output_file.write(resp.text)
     
-    time.sleep(10)
+    time.sleep(10) # ajaa scriptin suunnilleen 10 sekunnin välein hamaan tulevaisuuteen
